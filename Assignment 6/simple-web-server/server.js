03238542e9cf1d4e5399ba35991d7cd3c15ba16c
chauhanleekuhwr-ctrl <chauhanleeku.hwr@gmail.com>
2025-09-30 15:48:55 +0530

    Add files via upload

---
+++ Assignment 6/simple-web-server/server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = '';
+  let contentType = 'text/html';
+
+  if (req.url === '/' || req.url === '/home') {
+    filePath = path.join(__dirname, 'home.html');
+  } else if (req.url === '/about') {
+    filePath = path.join(__dirname, 'about.html');
+  } else if (req.url === '/contact') {
+    filePath = path.join(__dirname, 'contact.html');
+  } else if (req.url === '/styles.css') {
+    filePath = path.join(__dirname, 'styles.css');
+    contentType = 'text/css';
+  } else {
+    filePath = path.join(__dirname, '404.html');
+    res.statusCode = 404;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('500 - Internal Server Error');
+    } else {
+      res.writeHead(res.statusCode || 200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
